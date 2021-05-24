7771c2ca5175e79768733c181aca184abb31719b
Mateus Campos <mateus.camphos2000@gmail.com>
2021-05-24 09:13:07 -0300

    Método que imprime o resultado e o resto da divisão

---
+++ quotient_and_rest.rb
@@ -0,0 +1,8 @@
+# 1.7 Crie um script em Ruby que leia dois números inteiros, X e Y, e mostre o quociente e o resto da divisão de X por Y.
+def quotient_and_rest(x, y)
+  result = []
+  result << "#{x / y} "
+  result << "#{x % y} "
+   puts result
+end
+quotient_and_rest(10, 4)
