3fe1a8c3282bb345337dc4535f6b6b08620a2265
Mateus Campos <mateus.camphos2000@gmail.com>
2021-05-25 21:16:30 -0300

    Algoritmo que calcula a quantidade de dias vividos

---
+++ days_lived.rb
@@ -0,0 +1,7 @@
+# 1.8 Crie um script em Ruby que leia uma idade e calcule quantos dias essa pessoa já viveu. Considere que 1 ano tem 365 dias.
+
+def days_lived(age)
+  age * 365
+end
+puts days_lived(20)
+
