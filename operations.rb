2c1895f3ac1924c83bda5dd00d91d53ac126d757
Mateus Campos <mateus.camphos2000@gmail.com>
2021-05-25 21:51:02 -0300

    Correção para divisor 0

--- operations.rb
+++ operations.rb
@@ -4,6 +4,7 @@
  sum = n1 + n2
  product = n1 * n2
  quotient = n1 / n2.to_f
- ["um é #{sum}","o produto é #{product}","e o quociente é #{quotient}"]
+ quotient = 0 if n2.zero?
+ ["A soma é #{sum}","o produto é #{product}","e o quociente é #{quotient}"]
  end
  puts operations(4, 5)
